3658bc41d0c2dd8aebdff0c4b0940c3c047b07f1
Aneudy Adames <a4aneudy@gmail.com>
2021-01-12 14:52:09 -0500

    added a message when users havent added any movies to the nominees section

--- src/components/nominees/Nominees.js
+++ src/components/nominees/Nominees.js
@@ -1,13 +1,21 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../movie-card/MovieCard';
 import { NomineesContext } from '../../appContext';
+
 const Nominees = () => {
   const { nominees } = useContext(NomineesContext);
 
+  console.log(nominees);
   return (
     <section className='container'>
       <h2>Nominees</h2>
 
+      {nominees.length === 0 && (
+        <div className='add-nominees-msg '>
+          Go to <Link to='/'>Search Movies</Link> to add your nominees
+        </div>
+      )}
       <div className='nominees'>
         {nominees.map((movie) => (
           <MovieCard key={movie.imdbID} movie={movie} />
